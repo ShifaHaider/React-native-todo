c75fffcc27d890386e3ce8c2b229b8b9c6751c4b
Shifa Haider <shifa.haider065@gmail.com>
2018-11-03 07:07:13 +0500

    react-native-todo

--- todo.js
+++ todo.js
@@ -151,7 +151,7 @@ export default class Todo extends React.Component {
         );
     }
 }
-Hellooooooooooooooooooooooooooooooooooooooooooooo
+Helloooooooooooooooooooooooooooooooooooooooooooo
 const styles = StyleSheet.create({
     container: {
         flex: 1,
